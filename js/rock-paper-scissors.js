c2c840baab96a27a5dda2a5a8c0ec153d5f237e0
ZrC <94488901+AS-Coope@users.noreply.github.com>
2023-12-03 00:27:22 -0500

    Implement full game functionality WITHOUT complete testing

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,5 +1,6 @@
 // init variables
 let gameChoices = ["rock", "paper", "scissors"];
+let playGameResult = "";
 
 function getComputerChoice(){
 
@@ -16,13 +17,21 @@ function playGame(playerChoice, computerChoice){
 
   /* the strings of the computer's choice and the player input need 
    to be of the same case, or it won't properly check that they are the same, or not */
-  isPlayerChoiceValid = gameChoices.includes(playerChoice) ? true : false;
+  let isPlayerChoiceValid = gameChoices.includes(playerChoice) ? true : false;
 
-  if (isPlayerChoiceValid === false){
+  if (isPlayerChoiceValid === false ){
     console.log("You did not pick a suitable option. Please try again!");
     return playGame(getPlayerChoice(), computerChoice);
   }
-  return checkGameResult(playerChoice, computerChoice);
+
+  playGameResult = checkGameResult(playerChoice, computerChoice);
+  if (playGameResult === "Draw! Replay"){
+    console.log("Draw! Replay");
+    return playGame(getPlayerChoice(), getComputerChoice());
+  } else {
+    return playGameResult;
+  }
+
 }
 
 function checkGameResult(playerChoice, computerChoice){
@@ -40,8 +49,17 @@ function checkGameResult(playerChoice, computerChoice){
   } else if (playerChoice === gameChoices[2] && computerChoice === gameChoices[1]){
     return "You Win! Scissors beats Paper";
   } else {
-    console.log(playerChoice, computerChoice);
+    //console.log(playerChoice, computerChoice);
     return "Draw! Replay";
   }
 }
-console.log(playGame(getPlayerChoice(), getComputerChoice()));
+
+function game(){
+  let finalGameResult = "";
+  for (let iterator = 0; iterator < 5; iterator++){
+    finalGameResult = playGame(getPlayerChoice(), getComputerChoice());
+    console.log(finalGameResult);
+  }
+}
+game();
+//console.log(playGame(getPlayerChoice(), getComputerChoice()));
